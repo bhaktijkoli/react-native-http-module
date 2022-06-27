d1173342e2826ba73a7275e5c598916cc0e13c55
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-06-28 04:14:25 +0530

    feat: added get method

--- android/app/src/main/java/com/reactnativehttpmodule/HttpModule.java
+++ android/app/src/main/java/com/reactnativehttpmodule/HttpModule.java
@@ -1,12 +1,19 @@
 package com.reactnativehttpmodule;
 
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.io.IOException;
 import java.util.Map;
 import java.util.HashMap;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class HttpModule extends ReactContextBaseJavaModule {
     HttpModule(ReactApplicationContext context) {
@@ -15,5 +22,20 @@ public class HttpModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "HttpModule";
+    }
+
+    @ReactMethod
+    public void get(String url, Promise promise) {
+
+        OkHttpClient client = new OkHttpClient();
+        try {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            Response response = client.newCall(request).execute();
+            promise.resolve(response.body().string());
+        } catch (Exception e) {
+            promise.reject(e.getMessage());
+        }
     }
 }
