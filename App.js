77af594d8678af4b635beaabcf4192824a7f883c
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-07-02 06:52:26 +0530

    chore: http module create ssl client

--- App.js
+++ App.js
@@ -18,15 +18,44 @@ import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
+const CRT = `-----BEGIN CERTIFICATE-----
+MIID2jCCAsKgAwIBAgIUT9lChErS8YwrnS/6Pn33gnanjYwwDQYJKoZIhvcNAQEL
+BQAwgZ0xCzAJBgNVBAYTAklOMRMwEQYDVQQIDApNYWhhcmFzdHJhMQ4wDAYDVQQH
+DAVUaGFuZTESMBAGA1UECgwJS29pUmVhZGVyMQwwCgYDVQQLDANERVYxFTATBgNV
+BAMMDEJoYWt0aWogS29saTEwMC4GCSqGSIb3DQEJARYhYmhha3Rpai5qYXlhbnQu
+a29saUBrb2lyZWFkZXIuY29tMB4XDTIyMDYzMDA1NTkzMFoXDTMyMDYyNzA1NTkz
+MFowgZ0xCzAJBgNVBAYTAklOMRMwEQYDVQQIDApNYWhhcmFzdHJhMQ4wDAYDVQQH
+DAVUaGFuZTESMBAGA1UECgwJS29pUmVhZGVyMQwwCgYDVQQLDANERVYxFTATBgNV
+BAMMDEJoYWt0aWogS29saTEwMC4GCSqGSIb3DQEJARYhYmhha3Rpai5qYXlhbnQu
+a29saUBrb2lyZWFkZXIuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC
+AQEAtWboafBlApzbKlpuU+yKlGppnDf2G4a46a3SPstppiGiBESV3cefkTGvDjnj
+vf3+dPSsrqzSzwSgYe7hmLczIKUHFxtPHqc8JOjy/yzXKFLWBDgFWLUw8g5ghwTH
+7XrawLVuFX+PIC92tg8MB43R63+4W1jPaHc+DEqkNcDZWx5GyGZaLb5MCc1R0DcR
+7z7GeV2fjE++FwrgVW/DR8pPhktzv2TqpgkZq/U1jdahfGdqb16/D9WuRuG5FQyo
+oqX2ZHKTVNuDBtxGg4gQTn1jiiWlCDiscKMT69rwBPDvGFGt/75VLuAUEZLrzlEF
+jC3RMDUaduukETyxRCJIqJ0nfwIDAQABoxAwDjAMBgNVHRMEBTADAQH/MA0GCSqG
+SIb3DQEBCwUAA4IBAQAowwfCi0kYx3NCIRZMVJk2hpySj5gdRrj+pv9pJFHQFZHq
+1wRmbnnIqc0LDLHWASAY8fvBzd4eE/XOGVM17I1ift5Ep1i7Vb/KcIrUDm9lFefk
+9AuCkNWEy5hfkr7KJJZg7sSrYaMrAU+XZxcUsff/GqvCBBTD3H6nUqaH6q+QLEAy
+gXUfy6rAB+1NWRIu6Gwefl+GNS4aNxfmCaXPe9DF13Zqa+wAjyuJTADkAyyZPsRD
+o+j/NXv0ivmjI8X+mmQeeGfyznKEmBTI2lQzn1g9AXDwbZU9GKi/I8nhSr4CWjhg
+LbFzEklViuvA95/8EH0zxAzLJn6fZzKG8c4zVD55
+-----END CERTIFICATE-----
+`
+
 import { NativeModules } from 'react-native';
 
 const { HttpModule } = NativeModules;
 
 const App = () => {
   React.useEffect(() => {
-    HttpModule.get('http://192.168.0.102:8080/ping')
+    HttpModule.createSSLClient(CRT, '192.168.0.100')
+    HttpModule.get('https://192.168.0.100:8000/ping')
       .then((res) => {
         console.log(res);
+      })
+      .catch((err) => {
+        console.log(err)
       })
   }, [])
   return (
--- android/app/src/main/java/com/reactnativehttpmodule/HttpModule.java
+++ android/app/src/main/java/com/reactnativehttpmodule/HttpModule.java
@@ -1,21 +1,43 @@
 package com.reactnativehttpmodule;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.reactnativehttpmodule.utils.CustomTrust;
+import com.reactnativehttpmodule.utils.SslUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.security.cert.X509Certificate;
 import java.util.Map;
 import java.util.HashMap;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.tls.Certificates;
+import okhttp3.tls.HandshakeCertificates;
+import okhttp3.tls.HeldCertificate;
 
 public class HttpModule extends ReactContextBaseJavaModule {
+
+    private  final String TAG = "HttpModule";
+    private OkHttpClient client;
+
     HttpModule(ReactApplicationContext context) {
         super(context);
     }
@@ -25,14 +47,33 @@ public class HttpModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void get(String url, Promise promise) {
+    public void createSSLClient(String caStr, String hostnameStr) {
+        final X509Certificate CustomCertificationAuthority = Certificates.decodeCertificatePem(caStr);
+        HandshakeCertificates certificates = new HandshakeCertificates.Builder()
+                .addTrustedCertificate(CustomCertificationAuthority)
+                .addPlatformTrustedCertificates()
+                .build();
 
-        OkHttpClient client = new OkHttpClient();
+        HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return hostname.equals(hostnameStr);
+            }
+        };
+
+        this.client = new OkHttpClient.Builder()
+                .sslSocketFactory(certificates.sslSocketFactory(), certificates.trustManager())
+                .hostnameVerifier(hostnameVerifier)
+                .build();
+    }
+
+    @ReactMethod
+    public void get(String url, Promise promise) {
         try {
             Request request = new Request.Builder()
                     .url(url)
                     .build();
-            Response response = client.newCall(request).execute();
+            Response response = this.client.newCall(request).execute();
             promise.resolve(response.body().string());
         } catch (Exception e) {
             promise.reject(e.getMessage());
